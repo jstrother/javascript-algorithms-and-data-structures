4b2848fafbfc107f4cd3ef3780739688548a3d54
Jim Strother <jim.strother@gmail.com>
2020-12-19 16:01:01 -0600

    Solved: exercises/vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,39 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+// my initial solution
+// function vowels(str) {
+//   const vowelList = 'aeiou';
+//   let total = 0;
+
+//   for (let char of str.toLowerCase()) {
+//     for (let vowel of vowelList) {
+//       if (char === vowel) {
+//         total++;
+//       }
+//     }
+//   }
+
+//   return total;
+// }
+
+// solution #1 from course
+// function vowels(str) {
+//   const vowelList = ['a', 'e', 'i', 'o', 'u'];
+//   let total = 0;
+
+//   for (let char of str.toLowerCase()) {
+//     if (vowelList.includes(char)) {
+//       total++;
+//     }
+//   }
+
+//   return total;
+// }
