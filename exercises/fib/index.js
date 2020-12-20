a786efdd79cd628acbaebb4d0903e490e8a53e81
Jim Strother <jim.strother@gmail.com>
2020-12-20 14:42:58 -0600

    Solved: exercises/fib with memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,49 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) return cache[args];
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+function fib(n) {
+  if (n < 2) return n;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
+
+// my initial solution
+// function fib(n) {
+//   let result = 0;
+//   let prev = 0;
+//   let curr = 1;
+//   for (let i = 0; i < n; i++) {
+//     result = curr;
+//     curr += prev;
+//     prev = result;
+//   }
+//   return result;
+// }
+
+// solution #1 from course
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+//   return result[n];
+// }
